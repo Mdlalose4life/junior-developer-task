aaaa108dedf31a622d35e49dc05166bcec47033c
sbusisoShaper <sbusisomdlalose@thedigitalacademy.co.za>
2025-06-12 14:38:53 +0200

    handdling the empty data

--- api/webhook.js
+++ api/webhook.js
@@ -3,11 +3,11 @@ export default function handler(req, res) {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
 
-    const { data } = req.body;
-
-    if (data === null){
-        return res.status(400).json({error: '"data" must not be empty'})
+    if (!req.body){
+        return res.status(400).json({error: "Body Must Not Be Empty"})
     }
+
+    const { data } = req.body;
 
     if (typeof data !== 'string') {
         return res.status(400).json({ error: '"data" must be a string' });
